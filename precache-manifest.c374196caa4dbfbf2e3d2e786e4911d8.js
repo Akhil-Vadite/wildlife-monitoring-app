f0ab9a94f13f5e94decf2950901974c29aa3aac4
Akhil-Vadite <avadite@horizon.csueastbay.edu>
2024-09-29 17:32:29 -0700

    Updates

---
+++ precache-manifest.c374196caa4dbfbf2e3d2e786e4911d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f5ee879c1f7e40cd21420ea7d27970",
+    "url": "/wildlife-monitoring-app/index.html"
+  },
+  {
+    "revision": "048779dcfd300eefdd01",
+    "url": "/wildlife-monitoring-app/static/css/main.1520695e.chunk.css"
+  },
+  {
+    "revision": "92e23421ff612ea251ce",
+    "url": "/wildlife-monitoring-app/static/js/2.8851f76e.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/wildlife-monitoring-app/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "048779dcfd300eefdd01",
+    "url": "/wildlife-monitoring-app/static/js/main.6928b688.chunk.js"
+  },
+  {
+    "revision": "08919ee452cb5f000078",
+    "url": "/wildlife-monitoring-app/static/js/runtime~main.05982c42.js"
+  }
+]);
